70e77e7be2130b8594c7c47d2649d7602535464a
saumya varshneya <saumya810@gmail.com>
2014-11-12 16:21:04 +0530

    basic.h

---
+++ Tree/struct_tree/basic.h
@@ -0,0 +1,20 @@
+/*
+ * basic.h
+ *
+ *  Created on: Nov 12, 2014
+ *      Author: svarshne
+ */
+
+#ifndef BASIC_H_
+#define BASIC_H_
+
+struct node {
+	int data;
+	struct node *left;
+	struct node *right;
+
+};
+struct node *start;
+
+
+#endif /* BASIC_H_ */
