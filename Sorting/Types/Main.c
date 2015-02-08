7e190c76733fa5651cf46fad730f6a834a6bb36b
saumya810 <saumya810@gmail.com>
2015-02-08 23:13:14 +0530

    finally

---
+++ Sorting/Types/Main.c
@@ -0,0 +1,18 @@
+/*
+ * Main.c
+ *
+ *  Created on: Feb 8, 2015
+ *      Author: svarshne
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	//setbuf(stdout, NULL);
+	printf("hello hi bol k ");
+	return 1;
+}
