5cd44bc81c5320a3293edd0c1f45f9f208427f6d
saumya810 <saumya810@gmail.com>
2015-02-09 09:24:36 +0530

    Insertion sort added

--- Sorting/Types/Main.c
+++ Sorting/Types/Main.c
@@ -8,11 +8,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <basic.h>
 
 int main()
 {
 
-	//setbuf(stdout, NULL);
+	setbuf(stdout, NULL);
 	printf("hello hi bol k ");
+	options();
 	return 1;
 }
---
+++ Sorting/Types/Sorting_types.c
@@ -0,0 +1,71 @@
+/*
+ * Sorting_types.c
+ *
+ *  Created on: Feb 9, 2015
+ *      Author: svarshne
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <basic.h>
+
+int * Insertion(int * res);
+void print(int * res);
+void Merge();
+
+void options()
+{
+		int option,i;
+		int arr[8]={1,8,3,7,2,5,4,6},res[8];
+		printf("Choose the options :\n1. Insertions Sort \n2. Merge Sort");
+		scanf("%d",&option);
+		if(option==1)
+		{
+			for(i=0;i<8;i++)
+				res[i]=arr[i];
+			res=Insertion(res);
+			print(res);
+		}
+
+		else
+		{
+			Merge_Sort();
+		}
+
+}
+
+int * Insertion(int *arr)
+{
+
+	int i,key,j;
+	for(i=1;i<8;i++)
+	{
+		key=arr[i];
+		j=i-1;
+		while(j>=0 && arr[j]>key)
+		{
+			arr[j+1]=arr[j];
+			j--;
+		}
+		arr[j+1]=key;
+
+	}
+
+	return arr;
+
+}
+
+void Merge()
+{
+
+}
+
+void print(int * res)
+{
+	int i;
+	for(i=0;i<7;i++)
+	{
+		printf(" %d ",res[i]);
+	}
+	printf("\n\n *************************************\n\n");
+}
