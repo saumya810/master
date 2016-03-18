bf8406b396ec5cd00abab4898d28ba475959aeac
saumya810 <saumya810@gmail.com>
2016-03-19 02:06:45 +0530

    JAVA Sam

---
+++ Sam/src/pck1/First.java
@@ -0,0 +1,12 @@
+package pck1;
+import java.io.*;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heya");
+
+	}
+
+}
