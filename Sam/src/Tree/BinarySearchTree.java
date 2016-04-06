27da3816c5a3610fc7754d8862b5926cd07a7e60
saumya810 <saumya810@gmail.com>
2016-04-06 23:41:20 +0530

    All traversal

--- Sam/src/Tree/BinarySearchTree.java
+++ Sam/src/Tree/BinarySearchTree.java
@@ -6,10 +6,12 @@ public class BinarySearchTree {
 	public Node root;
 	
 	
-	public void Insert(Node NewNode){
+	public void Insert(int value){
+		Node NewNode = new Node(value);
 		if(this.root==null)
 		{
 			this.root=NewNode;
+			return;
 		}
 		
 		InsertNew(this.root,NewNode);
@@ -41,16 +43,44 @@ public class BinarySearchTree {
 		}
 	}
 	
-	public void Display(Node node)
+	public void Display_preorder(Node node)
 	{
 	  if(node!=null)
 	  {
 		  System.out.println(node.value);
 		  if(node.left!=null)
-			  Display(node.left);
+			  Display_preorder(node.left);
 		  if(node.right!=null)
-			  Display(node.right);
+			  Display_preorder(node.right);
 	  }
 	}
 	
+	public void Display_inorder(Node node)
+	{
+	  if(node!=null)
+	  {
+		  if(node.left!=null)
+			  Display_inorder(node.left);
+		  System.out.println(node.value);
+		  if(node.right!=null)
+			  Display_inorder(node.right);
+	  }
+	}
+	  
+	  public void Display_postorder(Node node)
+		{
+		  if(node!=null)
+		  {
+			  if(node.left!=null)
+				  Display_postorder(node.left);
+			 
+			  if(node.right!=null)
+				  Display_postorder(node.right);
+			  System.out.println(node.value);
+		  }
+	}
+	  
+	  
+	  
+	  
 }
--- Sam/src/Tree/TestTree.java
+++ Sam/src/Tree/TestTree.java
@@ -1,6 +1,31 @@
 package Tree;
 
 public class TestTree {
-
-	BinarySearchTree tree1=new BinarySearchTree();
+	
+	public static void main(String[] args){
+	
+		BinarySearchTree t1 = new BinarySearchTree();
+		t1.Insert(8);
+		t1.Insert(6);
+		t1.Insert(12);
+		t1.Insert(4);
+		t1.Insert(7);
+		t1.Insert(11);
+		t1.Insert(16);
+		t1.Insert(5);
+		t1.Insert(3);
+		t1.Display_preorder(t1.root);
+		t1.Display_inorder(t1.root);
+		t1.Display_postorder(t1.root);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
